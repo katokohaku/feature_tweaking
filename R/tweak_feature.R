d3a489fd3ca4dc5519a56ee451068c54d0c3dec6
KATO Kohaku <kato.satoshi.0@gmail.com>
2018-01-22 21:52:29 +0900

    add: sample with spam data set + misc fix

--- R/tweak_feature.R
+++ R/tweak_feature.R
@@ -1,3 +1,73 @@
+source("./R/utils.R")
+source("./R/parseRFrees.R")
+
+getRules <- function(forest, ntree=NULL, resample = FALSE){
+  
+  all.trees <- NULL
+  i.tree <- NULL
+  if(class(forest) == "randomForest"){
+    
+    if(is.null(ntree)){
+      i.tree <- 1:forest$ntree
+      catf("extracting all (%i of %i trees)", length(i.tree), forest$ntree)
+    } else {
+      maxk <- ifelse(ntree > forest$ntree, forest$ntree, ntree)
+      if(resample == FALSE){
+        i.tree <- 1:maxk
+        catf("extracting head(%i) of %i trees", length(i.tree), forest$ntree)
+      } else {
+        i.tree <- sample(forest$ntree, maxk, replace = FALSE)
+        catf("extracting sampled %i of %i trees", length(i.tree), forest$ntree)
+      }
+    }
+    
+    start.time <- Sys.time()
+    all.trees <- pforeach(k = i.tree, .c=list)({
+      getRules.randomForest(forest, k=k)
+    })
+    print(Sys.time() - start.time)
+  } 
+  if(class(forest) == "xgb.Booster"){
+    catf("Currently not compatible with xgboost")
+  }
+  
+  return(all.trees)
+}
+
+# get e-satisfactory instance of aim-leaf from all tree
+set.eSatisfactory <- function(forest, ntree=NULL, resample = FALSE, epsiron = 0.1) {
+  stopifnot(epsiron > 0)
+  
+  all.trees <- getRules(forest, ntree=ntree, resample=resample)
+  stopifnot(! is.null(all.trees))
+  
+  catf("set e-satisfactory instance (%i trees)", length(all.trees))
+  start.time <- Sys.time()
+  all.eTrees <- pforeach(tree.rules = all.trees, .c=list)({
+    tree.eRules <- NULL
+    for(cn in names(tree.rules)){
+      tree.eRules[[cn]] <- map(
+        tree.rules[[cn]]$path,
+        function(obj){ 
+          mutate(obj,
+                 eps = ifelse(lr=="<", -epsiron, +epsiron),
+                 e.satisfy = point + eps) %>% 
+            select(-node, -path.to)
+        }
+      )
+    }
+    return(tree.eRules)
+  })
+  print(Sys.time() - start.time)
+  
+  esforest <- list(forest = forest, trees = all.eTrees)
+  class(esforest) <- "forest.eSatisfactoryRules"
+  invisible(esforest)
+}
+
+# gr <- getRules.eSatisfy.rf(forest, ntree = 20, epsiron = 0.3)
+# gr %>% str(2)
+
 
 # calcurate e-satisfactory instance from predicted results ----------------
 tweak <- function(
@@ -9,10 +79,10 @@ tweak <- function(
   forest <- trees.eSatisfactory$forest
   estrees <- trees.eSatisfactory$trees
   nestree <- length(trees.eSatisfactory$trees)
-  catf("%1 instances were predicted by %i trees: ", NROW(newdata), nestree)
+  catf("%i instances were predicted by %i trees: ", NROW(newdata), nestree)
   
   pred.y <- predict(forest, newdata=newdata, predict.all=TRUE)
-  pred.Freq <- table(pred.y$aggregate) %>% data.frame
+  pred.Freq <- table(pred.y$aggregate)
   print(pred.Freq)
   
   .loop <- ifelse(.dopar, pforeach, npforeach)
@@ -92,7 +162,8 @@ plot.tweakedPopulation <- function(
   p <- stats %>% 
     ggplot(aes(x=reorder(var, value), y=value)) +
     geom_bar(stat = "identity") +
-    xlab("") + ylab("mean absolute effort") +
+    xlab("") + 
+    ylab("mean absolute effort") +
     coord_flip()
   
   if(type == "direction"){
@@ -102,7 +173,8 @@ plot.tweakedPopulation <- function(
       ggplot(aes(x=var, y=value)) +
       geom_hline(yintercept=0, colour = "red", size = 1.5) + 
       geom_boxplot() +
-      xlab("") + ylab("All direction of tweak") +
+      xlab("") + 
+      ylab("All direction of tweak") +
       coord_flip()
   }
   
@@ -111,7 +183,8 @@ plot.tweakedPopulation <- function(
     p <- stats %>% 
       ggplot(aes(x=reorder(var, nonZero), y=nonZero)) +
       geom_bar(stat = "identity") +
-      xlab("") + ylab("non-zerp frequency of feature tweaking") + 
+      xlab("") + 
+      ylab("non-zerp frequency of feature tweaking") + 
       coord_flip()
   }
   
@@ -121,7 +194,7 @@ plot.tweakedPopulation <- function(
 }
 
 # Plot individual suggestion (direction to modify)
-plot.suggest <- function(tweaked.X, k = 1, .order = FALSE, .nonzero.only = FALSE){
+plot.suggest <- function(tweaked.X, k = 1, .ordered = FALSE, .nonzero.only = FALSE){
   stopifnot(class(tweaked.X) == "tweaked.suggestion")
   tw.diff <- data.frame(tweaked.X$suggest - tweaked.X$original)
   
@@ -136,7 +209,9 @@ plot.suggest <- function(tweaked.X, k = 1, .order = FALSE, .nonzero.only = FALSE
   p <- p +
     geom_hline(yintercept=0, colour = "red", size = 1.5) + 
     geom_bar(stat = "identity")  +
-    xlab("") + ylab("directions of tweak") +
+    ggtitle(catf("instance #%i", k)) +
+    xlab("") + 
+    ylab("directions of tweak") +
     coord_flip()
   
   print(p)
